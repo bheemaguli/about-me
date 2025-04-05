340dfa0190f262492ca55eb47c805b71017b4cde
Sumanth Kashyap <sumanth.bhi@gmail.com>
2025-04-05 22:48:58 +0530

    [FIX] imports

--- src/features/stats/index.ts
+++ src/features/stats/index.ts
@@ -1,5 +1,4 @@
 export { ContributionDashboard } from "./ContributionDashboard";
 export { ContributionCalendar } from "./ContributionCalendar";
 export { HourlyStats } from "./HourlyStats";
-export { MonthlyStats } from "./MonthlyStats";
 export { WeekdayStats } from "./WeekdayStats";
